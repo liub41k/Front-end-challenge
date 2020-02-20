33be430e724fee9120af52b99e33f31e5e6f1195
Ihor Liubko <ihor_liubko@epam.com>
2020-02-20 13:33:15 +0200

    init

---
+++ src/js/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Example from './components/Example.js';
+
+const App = () => (
+	<article>
+		<h1>Hello, I’m your app, using the minimal React app base.</h1>
+		<p>
+			Head over to{' '}
+			<a href="//github.com/hankchizljaw/minimal-react-base">the repo</a> to
+			find out more info.
+		</p>
+		<Example text="Hi, pal 👋" label="Click me" />
+	</article>
+);
+
+export default App;
---
+++ static-assets.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const postcss = require('postcss');
+const atImport = require('postcss-import');
+const cssnano = require('cssnano');
+const isProduction = process.env.NODE_ENV === 'production';
+
+const paths = {
+	css: {
+		in: './src/css/global.css',
+		out: './dist/css/global.css'
+	},
+	html: {
+		in: './src/index.html',
+		out: './dist/index.html'
+	}
+};
+
+const runner = async () => {
+	const cssSrc = fs.readFileSync(paths.css.in, 'utf8');
+	const htmlSrc = fs.readFileSync(paths.html.in, 'utf8');
+
+	const {css} = await postcss()
+		.use(atImport())
+		.use(cssnano({sourcemap: !isProduction}))
+		.process(cssSrc, {
+			from: paths.css.in
+		});
+
+	fs.writeFileSync(paths.css.out, css);
+	fs.writeFileSync(paths.html.out, htmlSrc);
+
+	return true;
+};
+
+runner();
+module.exports = runner;
