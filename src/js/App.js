aa1aa606bbbfaf2608b2bd9913f45860ab0c7160
Ihor Liubko <ihor_liubko@epam.com>
2020-03-09 17:24:20 +0200

    toggle btn

--- src/js/App.js
+++ src/js/App.js
@@ -1,17 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import Example from './components/Example.js';
+const App = () => {
+	// wheather user prefers dark mode or not
+	const getPreferedColorScheme = () => {
+		if (!window.matchMedia) return; // feature detection => checks wheather user browser has matchMedia Func
 
-const App = () => (
-	<article>
-		<h1>Hello, I’m your app, using the minimal React app base.</h1>
-		<p>
-			Head over to{' '}
-			<a href="//github.com/hankchizljaw/minimal-react-base">the repo</a> to
-			find out more info.
-		</p>
-		<Example text="Hi, pal 👋" label="Click me" />
-	</article>
-);
+		return window.matchMedia('(prefers-color-scheme: dark)').matches;
+	};
+
+	const getInitialMode = () => {
+		const isReturningUser = 'dark' in localStorage;
+		const savedMode = JSON.parse(localStorage.getItem('dark'));
+		const userPrefersDark = getPreferedColorScheme();
+		// if mode was saved => dark/light
+		if (isReturningUser) {
+			return savedMode;
+		} else if (userPrefersDark) {
+			// if prefered color scheme is dark => dark
+			return true;
+		} else {
+			// otherwise => light
+			return false;
+		}
+	};
+
+	const [darkMode, setDarkMode] = useState(getInitialMode());
+
+	useEffect(() => {
+		localStorage.setItem('dark', JSON.stringify(darkMode));
+	}, [darkMode]);
+
+	return (
+		<div className={darkMode ? 'dark-mode' : 'light-mode'}>
+			<nav>
+				<div className="toggle-container">
+					<span style={{ color: darkMode ? 'gray' : 'yellow' }}>☀</span>
+					<span className="toggle">
+						<input
+							type="checkbox"
+							checked={darkMode}
+							onChange={() => setDarkMode(prevMode => !prevMode)}
+							className="checkbox"
+							id="checkbox"
+						/>
+						<label htmlFor="checkbox"></label>
+					</span>
+					<span style={{ color: darkMode ? 'slateblue' : 'gray' }}>☾</span>
+				</div>
+			</nav>
+			<main>
+				<h1>{darkMode ? 'Dark Mode' : 'Light Mode'}</h1>
+				<h2>Toggle the switch to see some magic happen!</h2>
+			</main>
+		</div>
+	);
+};
 
 export default App;
