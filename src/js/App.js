3593025913e8031ea0c754a3aeb41a78de36a427
Ihor Liubko <ihor_liubko@epam.com>
2020-07-03 14:43:06 +0300

    duotone card

--- src/js/App.js
+++ src/js/App.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import Example from './components/Example.js';
+const App = () => {
+	const [users, setUsers] = useState([]);
 
-const App = () => (
-	<article>
-		<h1>Hello, I’m your app, using the minimal React app base.</h1>
-		<p>
-			Head over to{' '}
-			<a href="//github.com/hankchizljaw/minimal-react-base">the repo</a> to
-			find out more info.
-		</p>
-		<Example text="Hi, pal 👋" label="Click me" />
-	</article>
-);
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((response) => response.json())
+			.then((json) => setUsers(json.slice(0, 3)));
+	}, []);
+
+	const cards = users.map(({ id, name, address: { city } }) => (
+		<li className="card-item" key={id}>
+			<img
+				src="https://cdn.shopify.com/s/files/1/0045/5104/9304/t/27/assets/AC_ECOM_SITE_2020_REFRESH_1_INDEX_M2_THUMBS-V2-1.jpg?v=8913815134086573859"
+				alt="img"
+			/>
+			<div className="card-item__info">
+				<div>{name}</div>
+				<div>{city}</div>
+			</div>
+		</li>
+	));
+
+	return (
+		<>
+			<h1 className="heading">Meet the team</h1>
+			<ul className="card-list">{cards}</ul>
+		</>
+	);
+};
 
 export default App;
